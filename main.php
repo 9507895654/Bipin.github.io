f63f150498325f9282c3f15df306565fa182b0fb
9507895654 <biharibipin2004@gmail.com>
2020-08-05 20:54:59 +0530

    my second website
    wow i am happy.

--- main.php
+++ main.php
@@ -1,58 +1,64 @@
-<?php
-
-include 'web.php';
-?>
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-
- <link rel="stylesheet" href="style.css">
-
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"> </div>
 </head>
 <body>
-<div class="container">
-    <div> <h2 class="logo"> devloper bipin </h2>
-    <div class="menu" id="maeu">
-    <ul>
-    <li> <a href="#" class="active"> home</a></li>
-    <li> <a href="#"> contact</a></li>
-    <li> <a href="about.php"> about</a></li>
-    <li> <a href="#"> Sorcecode</a></li>
-    </div>
-   
-    
-    <input type="submit" name="" id="bar" value="MENU" onclick="show()">
-    </div>
-    </div>
-    </ul>
-    
-    <div class="center">
-        <div class="gh"> 
-        <h1 class="letter"> Welcome to bipin kumar </h1>
-        <p> About me</p>
-        <h2> A website for <span class="bipin"> web devlopers</span></h2>
-</div>
-        <img src="w.png" alt="">
-        <!-- <img src="f.png" alt=""> -->
-        <img src="t.png" alt="">
-        <img src="y.png" alt="">
-    </div>
-    <footer>
-        <div class="ff">
+	<main>
+		<div class="main">
+		<div class="logo">
+			<h2>VIDEO </h2>
+		</div>
+		<div class="search">
+		<input type="search" name="search" placeholder="Search">
+		</div>
+		
+			 <div class="icon"> <a href="uploadvideo.php"><i class="fa fa-upload" aria-hidden="true"></i></a></div>
+		
+		<div class="username">
+			<h2>welcome:-</h2>
+		</div>
+		</div>
 
-    </footer>
+		<?php
+     include 'Connection.php';
+     $query =  "SELECT * FROM `video` order by id ASC ";
+     $queryfire = mysqli_query($con,$query);
+     $num = mysqli_num_rows($queryfire);
+     
 
-    <script>
-        function show(){
-            document.getElementByid('menu')
-            .style.display="block";
-        }
-    </script>
+     if($num>0){
+         while($product = mysqli_fetch_array($queryfire)){
+             ?>
+              <div class="dd">
+          
+                <div class="card">
+                    <video src="<?php echo $product['file'];?>" width="300px" height="300px"></video>
+                
+                    <div class="card_body">
+                        <h3> <?php echo $product['Channel Name']; ?> </h3>
+                        <h4> <?php echo $product['Title'] ?> </h4>
+                       </div>
+                </div>
+                </div>
+
+           
+        </div>
+ <?php
+
+         } 
+         
+     } 
+
+     ?>
+	
+     
 </body>
 </html>
+
+
+ 
